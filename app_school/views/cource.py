156005d36d6beea84b1f2320ef56623e303f16bb
Сергей Платонов <platonovpochta@gmail.com>
2023-10-14 16:42:03 +0200

    Homework 25.1 add Permissions

--- app_school/views/cource.py
+++ app_school/views/cource.py
@@ -1,10 +1,21 @@
-from django.db.models import Count
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from app_school.models import Course
+from app_school.permissions import IsModerator, IsOwner
 from app_school.serializers.course import CourseSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+    # permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [IsAuthenticated, IsModerator]
+        elif self.action in ['list', 'update', 'retrieve']:
+            self.permission_classes = [IsAuthenticated, IsModerator | IsOwner]
+        elif self.action == 'destroy':
+            self.permission_classes = [IsAuthenticated, IsOwner]
+        return super().get_permissions()
