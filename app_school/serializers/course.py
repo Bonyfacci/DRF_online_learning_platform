e18e0b0d2b886296a4c6eeb77c986ba745465dd8
Сергей Платонов <platonovpochta@gmail.com>
2023-10-17 23:16:40 +0200

    Homework 26.1 add Documentations add Pay_links_for_course

--- app_school/serializers/course.py
+++ app_school/serializers/course.py
@@ -3,12 +3,17 @@ from rest_framework import serializers
 from app_school.models import Course, Lesson
 from app_school.serializers.lesson import LessonSerializer
 from app_school.serializers.subscription import SubscriptionSerializer
+from app_school.services import create_a_payment_intent
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
     lesson_list = LessonSerializer(source='lessons', many=True, read_only=True)
     is_subscribed = SubscriptionSerializer(source='subscriptions', many=True, read_only=True)
+    pay_link = serializers.SerializerMethodField()
+
+    def get_pay_link(self, instance):
+        return create_a_payment_intent(Course.objects.get(pk=instance.pk))
 
     def get_lesson_count(self, instance):
         return instance.lessons.count()
